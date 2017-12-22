861369a24e38d1e1f96761d201a375054e8f112a
EfforiaKnight <tualet00@gmail.com>
2017-12-22 18:31:37 +0200

    WIP

--- fizzygit.fish
+++ fizzygit.fish
@@ -34,7 +34,7 @@ function fizzygit
         git rev-parse --is-inside-work-tree >/dev/null
     end
 
-    function glo
+    function glo -d "Pretty git log with fzf"
         inside_work_tree; or return 1
         set -l fancy (__diff_so_fancy)
 
@@ -45,11 +45,45 @@ function fizzygit
         set -l gitlog "git log --graph --color=always --pretty=format:'%Cred%h%Creset -%C(auto)%d% %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
 
         if [ $FZF_TMUX -eq 1 ]
-            set show "$grepsha |xargs -I {} tmux split-window -p 100 -c '#{pane_current_path}' '$gitshow'{}'$fancy |less -R'"
+            set -q FZF_TMUX_SPLIT_W; or set FZF_TMUX_SPLIT_W "tmux split-window -p 100 -c '#{pane_current_path}'"
+            set show "$grepsha |xargs -I {} $FZF_TMUX_SPLIT_W '$gitshow'{}'$fancy |less -R'"
         else
-            set show "$grepsha |xargs -I {} $gitshow {} $fancy |less -R"
+            set show "$cmd |less -R"
         end
 
         eval "$gitlog |" (__fzf_fizzycmd) ' -e +s --tiebreak=index --bind="enter:execute($show)" --preview="$cmd"'
+    end
+
+    function gd -d "Git diff with fzf"
+        inside_work_tree; or return 1
+        set -l fancy (__diff_so_fancy)
+
+        set -l gitdiff "git diff --color=always -- "
+
+        set -l cmd "$gitdiff {} $fancy"
+        set -l gitls "git ls-files --modified"
+
+        if [ $FZF_TMUX -eq 1 ]
+            set -q FZF_TMUX_SPLIT_W; or set FZF_TMUX_SPLIT_W "tmux split-window -p 100 -c '#{pane_current_path}'"
+            set show "$FZF_TMUX_SPLIT_W '$gitdiff'{}'$fancy |less -R'"
+        else
+            set show "$cmd |less -R"
+        end
+
+        eval "$gitls |" (__fzf_fizzycmd) ' -e -0 --bind="enter:execute($show)" --preview="$cmd"'
+    end
+
+    function ga -d "Git add with fzf"
+        inside_work_tree; or return 1
+        set -l fancy (__diff_so_fancy)
+
+        set -l gitdiff "git diff --color=always -- "
+
+        set -l cmd "$gitdiff {-1} $fancy"
+        set -l gitstatus "git -c color.status=always status --short | grep 31m | awk '{printf "[%10s]  ", $1; $1=""; print $0}'"
+
+        set -l files (eval "$gitstatus |" (__fzf_fizzycmd) ' -e -0 -m --nth 2..,.. --preview="$cmd" | cut -d] -f2 | sed "s/.* -> //"')
+        echo $files
+        # [ -n $files ]; and echo $files |xargs -t -I {} git add {}; and git status
     end
 end
